b7900cf0db44f52eee6367c4800e969318a0c837
Venkata Subba Reddy Dunnuthula <dvsreddy221@gmail.com>
2021-07-20 13:53:31 +0530

    Add project files.

---
+++ TextToSpeechConverter/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace TextToSpeechConverter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.btn_Speak = new System.Windows.Forms.Button();
+            this.btn_Resume = new System.Windows.Forms.Button();
+            this.btn_Pause = new System.Windows.Forms.Button();
+            this.btn_Stop = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(18, 18);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(882, 310);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // btn_Speak
+            // 
+            this.btn_Speak.Location = new System.Drawing.Point(18, 338);
+            this.btn_Speak.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btn_Speak.Name = "btn_Speak";
+            this.btn_Speak.Size = new System.Drawing.Size(118, 45);
+            this.btn_Speak.TabIndex = 1;
+            this.btn_Speak.Text = "Speak";
+            this.btn_Speak.UseVisualStyleBackColor = true;
+            this.btn_Speak.Click += new System.EventHandler(this.btn_Speak_Click);
+            // 
+            // btn_Resume
+            // 
+            this.btn_Resume.Location = new System.Drawing.Point(146, 338);
+            this.btn_Resume.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btn_Resume.Name = "btn_Resume";
+            this.btn_Resume.Size = new System.Drawing.Size(116, 45);
+            this.btn_Resume.TabIndex = 2;
+            this.btn_Resume.Text = "Resume";
+            this.btn_Resume.UseVisualStyleBackColor = true;
+            this.btn_Resume.Click += new System.EventHandler(this.btn_Resume_Click);
+            // 
+            // btn_Pause
+            // 
+            this.btn_Pause.Location = new System.Drawing.Point(270, 338);
+            this.btn_Pause.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btn_Pause.Name = "btn_Pause";
+            this.btn_Pause.Size = new System.Drawing.Size(132, 45);
+            this.btn_Pause.TabIndex = 3;
+            this.btn_Pause.Text = "Pause";
+            this.btn_Pause.UseVisualStyleBackColor = true;
+            this.btn_Pause.Click += new System.EventHandler(this.btn_Pause_Click);
+            // 
+            // btn_Stop
+            // 
+            this.btn_Stop.Location = new System.Drawing.Point(411, 338);
+            this.btn_Stop.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btn_Stop.Name = "btn_Stop";
+            this.btn_Stop.Size = new System.Drawing.Size(118, 45);
+            this.btn_Stop.TabIndex = 4;
+            this.btn_Stop.Text = "Stop";
+            this.btn_Stop.UseVisualStyleBackColor = true;
+            this.btn_Stop.Click += new System.EventHandler(this.btn_Stop_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(548, 338);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(163, 45);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "ConvertToAudio";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(949, 482);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btn_Stop);
+            this.Controls.Add(this.btn_Pause);
+            this.Controls.Add(this.btn_Resume);
+            this.Controls.Add(this.btn_Speak);
+            this.Controls.Add(this.richTextBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Text to Speech Converter";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button btn_Speak;
+        private System.Windows.Forms.Button btn_Resume;
+        private System.Windows.Forms.Button btn_Pause;
+        private System.Windows.Forms.Button btn_Stop;
+        private System.Windows.Forms.Button button1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    }
+}
+
---
+++ TextToSpeechConverter/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Speech;
+using System.Speech.Synthesis;
+using System.IO;
+using System.Globalization;
+
+namespace TextToSpeechConverter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //SpeechSynthesizer Class Provides access to the functionality of an installed a speech synthesis engine.
+        SpeechSynthesizer speechSynthesizerObj;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            speechSynthesizerObj = new SpeechSynthesizer();
+            btn_Resume.Enabled = false;
+            btn_Pause.Enabled = false;
+            btn_Stop.Enabled = false;
+        }
+        
+
+        private void btn_Speak_Click(object sender, EventArgs e)
+        {
+            //Disposes the SpeechSynthesizer object 
+            speechSynthesizerObj.Dispose();
+            if(richTextBox1.Text!="")
+            {
+                speechSynthesizerObj = new SpeechSynthesizer();
+                //Asynchronously speaks the contents present in RichTextBox1
+                speechSynthesizerObj.SelectVoiceByHints(VoiceGender.Female, VoiceAge.Teen, 0, CultureInfo.GetCultureInfo("en-GB"));
+                speechSynthesizerObj.SpeakAsync(richTextBox1.Text);
+     
+                btn_Pause.Enabled = true;
+                btn_Stop.Enabled = true;              
+            }
+        }
+       
+        private void btn_Pause_Click(object sender, EventArgs e)
+        {
+            if(speechSynthesizerObj!=null)
+            {
+                //Gets the current speaking state of the SpeechSynthesizer object.
+                if(speechSynthesizerObj.State==SynthesizerState.Speaking)
+                {
+                    //Pauses the SpeechSynthesizer object.
+                    speechSynthesizerObj.Pause();
+                    btn_Resume.Enabled = true;
+                    btn_Speak.Enabled = false;
+                }
+            }
+        }
+
+        private void btn_Resume_Click(object sender, EventArgs e)
+        {
+            if (speechSynthesizerObj != null)
+            {
+                if (speechSynthesizerObj.State == SynthesizerState.Paused)
+                {
+                    //Resumes the SpeechSynthesizer object after it has been paused.
+                    speechSynthesizerObj.Resume();
+                    btn_Resume.Enabled = false;
+                    btn_Speak.Enabled = true;
+                }
+            }
+        }
+
+        private void btn_Stop_Click(object sender, EventArgs e)
+        {
+            if(speechSynthesizerObj!=null)
+            {
+                //Disposes the SpeechSynthesizer object 
+                speechSynthesizerObj.Dispose();
+                btn_Speak.Enabled = true;
+                btn_Resume.Enabled = false;
+                btn_Pause.Enabled = false;
+                btn_Stop.Enabled = false;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            speechSynthesizerObj.Dispose();
+            if (richTextBox1.Text != "")
+            {
+                string result = TextToSpeech(richTextBox1.Text);
+                string base64Text = "data:audio/wav;base64,"+ result;
+                Clipboard.SetText(base64Text);
+                MessageBox.Show("Text Converted To Audio and Copied Stream");
+
+            }
+               
+        }
+        public string TextToSpeech(string text)
+        {
+            MemoryStream audioStream = new MemoryStream();
+            string result;
+            using (var synthesizer = new SpeechSynthesizer())
+            {
+                synthesizer.SetOutputToWaveStream(audioStream);
+                synthesizer.SelectVoiceByHints(VoiceGender.Female, VoiceAge.Teen, 0, CultureInfo.GetCultureInfo("en-GB"));
+                synthesizer.Speak(text);
+                byte[] bytes = audioStream.ToArray();
+                result = Convert.ToBase64String(bytes);
+            }
+            return result;
+         }
+    }
+}
